925daa5fa523991b9d5dea41befc2423c295102a
guilhermedel <113988360+guilhermedel@users.noreply.github.com>
2022-09-26 19:28:01 -0300

    resolvendo

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -8,18 +8,19 @@ public class Programa {
         // Dois objetos da classe Cliente
         Scanner sc = new Scanner(System.in);
         for (int i = 1; i<=2; i++) {
-            System.out.println("Digite o nome "+i+" :");
+            System.out.println("Digite o nome do cliente"+i+" :");
             String nome = sc.nextLine();
-            System.out.println("Digite o cpf "+i+" :");
+            System.out.println("Digite o cpf do cliente"+i+" :");
             Integer cpf = sc.nextInt();
-            System.out.println("Digite o telefone "+i+" :");
-            String telefone = sc.nextLine();
-            Cliente cliente = new Cliente(cpf, nome, telefone);
+            System.out.println("Digite o enderço do cliente"+i+" :");
+            String endereco = sc.nextLine();
+            Cliente cliente = new Cliente(cpf, nome, endereco);
             System.out.println(cliente.toString());
         }
         for (int i=1;i<=3;i++){
             System.out.println("Digite o codigo "+i+" :");
             int codigo = Integer.parseInt(sc.nextLine());
+            
             System.out.println("Digite o nome "+i+" :");
             String nome = sc.nextLine();
             System.out.println("Digite o preco "+i+" :");
